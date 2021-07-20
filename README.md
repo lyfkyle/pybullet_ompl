62bf99d7c65f7056ba6651c20e64682f30ebbbbc
lyf44 <luyunfan44@gmail.com>
2021-07-20 13:07:10 +0800

    v0.1

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # pybullet_ompl
+This repo provides interface to use OMPL for motion planning inside PyBullet. It uses OMPL python bindings.
+
+# Environment
+Tested with:
+**Python 3.8**
+**Ubuntu18.04**
+
+# Installation instructions:
+
+## Install dependencies of OMPL
+https://github.com/ompl/ompl/blob/main/doc/markdown/installPyPlusPlus.md
+
+## Install OMPL from source
+It is very important that you compile ompl with the correct python version with the CMake flag.
+```
+git clone https://github.com/ompl/ompl.git
+mkdir build/Release
+cd build/Release
+cmake ../.. -DPYTHON_EXEC=/path/to/python-X.Y # This is important!!! Make sure you are pointing to the correct python version.
+make -j 4 update_bindings # replace "4" with the number of cores on your machine. This step takes some time.
+make -j 4 # replace "4" with the number of cores on your machine
+```
+
+## Install Pybullet
+Just install pybullet normally.
+```
+pip install pybullet
+```
+
+# Demo
+This demo plans whole-body motion of a planar 4-link snake-like robot.
+```
+python3 maze_demo.py
+```
+
+# Additional Information
+1. Currently only RRT and BITstar are enabled and tested. But all planners in OMPL should work. Just add them in the set_planner API in PbOMPL class.
+2. To work with other robot, you might need to inherit from PbOMPLRobot class in PbOMPL and override several functionalities. Refer to my_planar_robot.py for an example.
---
+++ pb_ompl.py
@@ -0,0 +1,255 @@
+try:
+    from ompl import util as ou
+    from ompl import base as ob
+    from ompl import geometric as og
+except ImportError:
+    # if the ompl module is not in the PYTHONPATH assume it is installed in a
+    # subdirectory of the parent directory called "py-bindings."
+    from os.path import abspath, dirname, join
+    import sys
+    sys.path.insert(0, join(dirname(dirname(abspath(__file__))), 'ompl/py-bindings'))
+    from ompl import util as ou
+    from ompl import base as ob
+    from ompl import geometric as og
+import pybullet as p
+import utils
+import time
+from itertools import product
+
+INTERPOLATE_NUM = 500
+DEFAULT_PLANNING_TIME = 1.0
+
+class PbOMPLRobot():
+    '''
+    To use with Pb_OMPL. You need to construct a instance of this class and pass to PbOMPL.
+
+    Note:
+    This parent class by default assumes that all joints are acutated and should be planned. If this is not your desired
+    behaviour, please write your own inheritated class that overrides respective functionalities.
+    '''
+    def __init__(self, id) -> None:
+        # Public attributes
+        self.id = id
+        self.num_dim = p.getNumJoints(id) # by default, all joints are actuated
+        self.joint_idx= list(range(self.num_dim))
+
+        self.joint_bounds = [[-float('inf'), float('inf')]] * self.num_dim
+        self.reset()
+
+    def get_joint_bounds(self):
+        '''
+        Get joint bounds.
+        By default, read from pybullet
+        '''
+        for i, joint_id in enumerate(self.joint_idx):
+            joint_info = p.getJointInfo(self.id, joint_id)
+            if joint_info.jointLowerLimit < joint_info.jointUpperLimit:
+                self.joint_bounds[i][0] = joint_info.jointLowerLimit
+                self.joint_bounds[i][1] = joint_info.jointUpperLimit
+        return self.joint_bounds
+
+    def get_cur_state(self):
+        return self.state
+
+    def set_state(self, state):
+        '''
+        Set robot state.
+        To faciliate collision checking
+        Args:
+            state: list[Float], joint values of robot
+        '''
+        self._set_joint_positions(self.joint_idx, state)
+        self.state = state
+
+    def reset(self):
+        '''
+        Reset robot state
+        Args:
+            state: list[Float], joint values of robot
+        '''
+        state = [0] * self.num_dim
+        self._set_joint_positions(self.joint_idx, state)
+        self.state = state
+
+    def _set_joint_positions(self, joints, positions):
+        for joint, value in zip(joints, positions):
+            p.resetJointState(self.id, joint, value, targetVelocity=0)
+
+class PbStateSpace(ob.RealVectorStateSpace):
+    def __init__(self, num_dim) -> None:
+        super().__init__(num_dim)
+        self.num_dim = num_dim
+
+    def allocStateSampler(self):
+        '''
+        This will be called by the internal OMPL planner
+        '''
+        # WARN: This will cause problems if the underlying planner is multi-threaded!!!
+        if self.state_sampler:
+            return self.state_sampler
+
+        # when ompl planner calls this, we will return our sampler
+        return self.allocDefaultStateSampler()
+
+    def set_state_sampler(self, state_sampler):
+        '''
+        Optional, Set custom state sampler.
+        '''
+        self.state_sampler = state_sampler
+
+class PbOMPL():
+    def __init__(self, robot, obstacles = []) -> None:
+        '''
+        Args
+            robot: A PbOMPLRobot instance.
+            obstacles: list of obstacle ids. Optional.
+        '''
+        self.robot = robot
+        self.robot_id = robot.id
+        self.obstacles = obstacles
+        print(self.obstacles)
+
+        self.space = PbStateSpace(robot.num_dim)
+
+        bounds = ob.RealVectorBounds(robot.num_dim)
+        joint_bounds = self.robot.get_joint_bounds()
+        for i, bound in enumerate(joint_bounds):
+            bounds.setLow(i, bound[0])
+            bounds.setHigh(i, bound[1])
+        self.space.setBounds(bounds)
+
+        self.ss = og.SimpleSetup(self.space)
+        self.ss.setStateValidityChecker(ob.StateValidityCheckerFn(self.is_state_valid))
+        self.si = self.ss.getSpaceInformation()
+        # self.si.setStateValidityCheckingResolution(0.005)
+        # self.collision_fn = pb_utils.get_collision_fn(self.robot_id, self.robot.joint_idx, self.obstacles, [], True, set(),
+        #                                                 custom_limits={}, max_distance=0, allow_collision_links=[])
+
+        self.set_obstacles(obstacles)
+        self.set_planner("RRT") # RRT by default
+
+    def set_obstacles(self, obstacles):
+        self.obstacles = obstacles
+
+        # update collision detection
+        self.setup_collision_detection(self.robot, self.obstacles)
+
+    def add_obstacles(self, obstacle_id):
+        self.obstacles.append(obstacle_id)
+
+    def remove_obstacles(self, obstacle_id):
+        self.obstacles.remove(obstacle_id)
+
+    def is_state_valid(self, state):
+        # satisfy bounds TODO
+        # if not pb_utils.all_between(lower_limits, state, upper_limits):
+            # pass
+            # print(lower_limits, q, upper_limits)
+            # print('Joint limits violated')
+            # return True
+
+        self.robot.set_state(self.state_to_list(state))
+        for link1, link2 in self.check_link_pairs:
+            if utils.pairwise_link_collision(self.robot_id, link1, self.robot_id, link2):
+                #print(get_body_name(body), get_link_name(body, link1), get_link_name(body, link2))
+                return False
+        for body1, body2 in self.check_body_pairs:
+            if utils.pairwise_collision(body1, body2):
+                # print('body collision', body1, body2)
+                #print(get_body_name(body1), get_body_name(body2))
+                return False
+        return True
+
+    def setup_collision_detection(self, robot, obstacles, self_collisions = True, allow_collision_links = []):
+        self.check_link_pairs = utils.get_self_link_pairs(robot.id, robot.joint_idx) if self_collisions else []
+        moving_links = frozenset(
+            [item for item in utils.get_moving_links(robot.id, robot.joint_idx) if not item in allow_collision_links])
+        moving_bodies = [(robot.id, moving_links)]
+        self.check_body_pairs = list(product(moving_bodies, obstacles))
+        print(self.check_body_pairs)
+
+    def set_planner(self, planner_name):
+        '''
+        TODO Add your planner here!!
+        '''
+        if planner_name == "RRT":
+            self.planner = og.RRT(self.ss.getSpaceInformation())
+        elif planner_name == "BITstar":
+            self.planner = og.BITstar(self.ss.getSpaceInformation())
+        else:
+            print("{} not recognized, please add it first".format(planner_name))
+            return
+
+        self.ss.setPlanner(self.planner)
+
+    def plan_start_goal(self, start, goal, allowed_time = DEFAULT_PLANNING_TIME):
+        print("start_planning")
+        print(self.planner.params())
+
+        orig_robot_state = self.robot.get_cur_state()
+
+        # set the start and goal states;
+        s = ob.State(self.space)
+        g = ob.State(self.space)
+        for i in range(len(start)):
+            s[i] = start[i]
+            g[i] = goal[i]
+
+        self.ss.setStartAndGoalStates(s, g)
+
+        # attempt to solve the problem within allowed planning time
+        solved = self.ss.solve(allowed_time)
+        res = False
+        sol_path_list = []
+        if solved:
+            print("Found solution:")
+            # print the path to screen
+            sol_path_geometric = self.ss.getSolutionPath()
+            sol_path_geometric.interpolate(INTERPOLATE_NUM)
+            sol_path_states = sol_path_geometric.getStates()
+            sol_path_list = [self.state_to_list(state) for state in sol_path_states]
+            print(len(sol_path_list))
+            # print(sol_path_list)
+            for sol_path in sol_path_list:
+                self.is_state_valid(sol_path)
+            res = True
+        else:
+            print("No solution found")
+
+        # reset robot state
+        self.robot.set_state(orig_robot_state)
+        return res, sol_path_list
+
+    def plan(self, goal, allowed_time = DEFAULT_PLANNING_TIME):
+        '''
+        plan a path to gaol from current robot state
+        '''
+        start = self.robot.get_cur_state()
+        return self.plan_start_goal(start, goal, allowed_time=allowed_time)
+
+    def execute(self, path):
+        '''
+        Execute a planned plan. Will visualize in pybullet.
+        Use robot.set_state(), meaning that the simulator will simply reset robot's state WITHOUT any dynamics simulation. Since the
+        path is collision free, this is somewhat acceptable.
+        Args:
+            path: list[state], a list of state
+        '''
+        for q in path:
+            self.robot.set_state(q)
+            p.stepSimulation()
+            time.sleep(0.01)
+
+    # -------------
+    # Configurations
+    # ------------
+
+    def set_state_sampler(self, state_sampler):
+        self.space.set_state_sampler(state_sampler)
+
+    # -------------
+    # Util
+    # ------------
+
+    def state_to_list(self, state):
+        return [state[i] for i in range(self.robot.num_dim)]
